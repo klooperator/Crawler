23fdb45397fded0e4935c5096292efe40c30cee4
klooperator <klooperator@gmail.com>
2017-03-12 11:52:57 +0100

    uuu

--- app/src/main/java/com/droid/klo/crawler/MainActivity.java
+++ app/src/main/java/com/droid/klo/crawler/MainActivity.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+
     }
 }
